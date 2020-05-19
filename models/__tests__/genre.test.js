f3456d0b18d4d40e0ba58f853e415893b6316bc3
Zett-8 <kame0008@gmail.com>
2020-05-19 12:57:33 +0200

    added test for genre model

---
+++ models/__tests__/genre.test.js
@@ -0,0 +1,33 @@
+const Genre = require('../').Genre
+
+beforeEach(() => Genre.destroy({ truncate: true }))
+
+describe('Genre model', () => {
+  it('create test', async () => {
+    const current = await Genre.findAll()
+    expect(current).toHaveLength(0)
+
+    await Genre.create({ genre_name: 'test' })
+    const genres = await Genre.findAll()
+    expect(genres).toHaveLength(1)
+    expect(genres[0].genre_name).toBe('test')
+  })
+
+  it('modify test', async () => {
+    await Genre.create({ genre_name: 'test' })
+    await Genre
+      .findOne({ where: { genre_name: 'test' }})
+      .then(genre => genre.update({ genre_name: 'modified' }))
+
+    const modifiedGenre = await Genre.findAll()
+    expect(modifiedGenre[0].genre_name).toBe('modified')
+  })
+
+  it('delete test', async () => {
+    await Genre.create({ genre_name: 'test' })
+    await Genre.findOne({ where: { genre_name: 'test'}}).then(genre => genre.destroy())
+
+    const genres = await Genre.findAll()
+    expect(genres).toHaveLength(0)
+  })
+})
--- models/__tests__/index.test.js
+++
@@ -1,5 +0,0 @@
-describe('test for test', () => {
-  it('test', () => {
-    expect(true).toBe(true)
-  })
-})
