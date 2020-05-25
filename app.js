3f62f19704340ce4e2f64b41c314f3f658d2066f
Zett-8 <kame0008@gmail.com>
2020-05-25 22:15:23 +0200

    rebase master

--- app.js
+++ app.js
@@ -22,12 +22,16 @@ app.use('/$', (req, res) => res.send('hello world'))
 app.use('/users', usersRouter)
 app.use('/api', passport.authenticate('jwt', { session: false }), indexRouter)
 
-app.use('/graphql',
-  process.env.NODE_ENV === 'production' ? passport.authenticate('jwt', { session: false }) : (req, res, next) => next(), graphqlHTTP({
-  schema: {},
-  graphiql: true
-}))
-
+app.use(
+  '/graphql',
+  process.env.NODE_ENV === 'production'
+    ? passport.authenticate('jwt', { session: false })
+    : (req, res, next) => next(),
+  graphqlHTTP({
+    schema: {},
+    graphiql: true,
+  })
+)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
